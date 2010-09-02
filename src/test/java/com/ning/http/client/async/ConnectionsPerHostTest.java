fa81994f15c34f19a13c79a1bc262b156c6176e7
Jeffrey Beorse <jeffisveryhungry@dhcp-171-68-116-240.cisco.com>
2010-09-02 13:55:33 -0700

    Updated unit test to include more cases. STILL NOT FINISHED

--- src/test/java/com/ning/http/client/async/ConnectionsPerHostTest.java
+++ src/test/java/com/ning/http/client/async/ConnectionsPerHostTest.java
@@ -21,6 +21,7 @@ import com.ning.http.client.AsyncHttpClientConfig;
 import com.ning.http.client.HttpResponseBodyPart;
 import com.ning.http.client.HttpResponseHeaders;
 import com.ning.http.client.HttpResponseStatus;
+import com.ning.http.client.Response;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 import org.testng.annotations.Test;
@@ -34,6 +35,7 @@ import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.Future;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertFalse;
@@ -48,27 +50,116 @@ import static org.testng.Assert.fail;
 public class ConnectionsPerHostTest extends AbstractBasicTest {
 
     @Test(groups = "standalone")
-    public void testConnectionsPerHost() throws IOException {
-		AsyncHttpClientConfig cf = new AsyncHttpClientConfig.Builder().setKeepAlive(true).setMaximumConnectionsPerHost(3).build();
+    public void testValidNumConnections() throws IOException {
+		AsyncHttpClientConfig cf = new AsyncHttpClientConfig.Builder().setKeepAlive(true).setMaximumConnectionsPerHost(2).build();
         AsyncHttpClient ahc = new AsyncHttpClient(cf);
 		
 		try{
-			ahc.prepareGet(getTargetUrl()).execute();
-			ahc.prepareGet(getTargetUrl()).execute();
-			ahc.prepareGet(getTargetUrl()).execute();
+			Future<Response> f1 = ahc.prepareGet(getTargetUrl()).execute();
+			Future<Response> f2 = ahc.prepareGet(getTargetUrl()).execute();
+			while(!f1.isDone() && !f2.isDone());
 		} catch(Exception e){
 			fail("Exception occured while opening connections to host", e);
 		}
+		
+		//Should be no log warnings here
 		
 		try{
-			ahc.prepareGet(getTargetUrl()).execute();
-			ahc.prepareGet(getTargetUrl()).execute();
-			ahc.prepareGet(getTargetUrl()).execute();
-			ahc.prepareGet(getTargetUrl()).execute();
+			Future<Response> f3 = ahc.prepareGet(getTargetUrl()).execute();
+			Future<Response> f4 = ahc.prepareGet(getTargetUrl()).execute();
+			while(!f3.isDone() && !f4.isDone());
 		} catch(Exception e){
 			fail("Exception occured while opening connections to host", e);
 		}
 		
-		log.trace("");
+		//Should be no log warnings here
+
+    }
+	
+	@Test(groups = "standalone")
+    public void testTooManyConnections() throws IOException {
+		AsyncHttpClientConfig cf = new AsyncHttpClientConfig.Builder().setKeepAlive(true).setMaximumConnectionsPerHost(2).build();
+        AsyncHttpClient ahc = new AsyncHttpClient(cf);
+		
+		try{
+			Future<Response> f1 = ahc.prepareGet(getTargetUrl()).execute();
+			Future<Response> f2 = ahc.prepareGet(getTargetUrl()).execute();
+			while(!f1.isDone() && !f2.isDone());
+		} catch(Exception e){
+			fail("Exception occured while opening connections to host", e);
+		}
+		
+		//Should be no log warnings here
+		
+		try{
+			Future<Response> f3 = ahc.prepareGet(getTargetUrl()).execute();
+			Future<Response> f4 = ahc.prepareGet(getTargetUrl()).execute();
+			Future<Response> f5 = ahc.prepareGet(getTargetUrl()).execute();
+			while(!f3.isDone() && !f4.isDone() && !f5.isDone() );
+		} catch(Exception e){
+			fail("Exception occured while opening connections to host", e);
+		}
+		
+		//Should be log warnings here
+		
+    }
+	
+	@Test(groups = "standalone")
+    public void testValidNumConnectionsMultiHost() throws IOException {
+		AsyncHttpClientConfig cf = new AsyncHttpClientConfig.Builder().setKeepAlive(true).setMaximumConnectionsPerHost(2).build();
+        AsyncHttpClient ahc = new AsyncHttpClient(cf);
+		
+		try{
+			Future<Response> f1 = ahc.prepareGet(getTargetUrl()).execute();
+			Future<Response> f2 = ahc.prepareGet(getTargetUrl()).execute();
+			Future<Response> f3 = ahc.prepareGet("http://www.google.com/").execute();
+			while(!f1.isDone() && !f2.isDone() && !f3.isDone());
+		} catch(Exception e){
+			fail("Exception occured while opening connections to host", e);
+		}
+		
+		//Should be no log warnings here
+		
+		try{
+			Future<Response> f4 = ahc.prepareGet(getTargetUrl()).execute();
+			Future<Response> f5 = ahc.prepareGet(getTargetUrl()).execute();
+			Future<Response> f6 = ahc.prepareGet("http://www.google.com/").execute();
+			while(!f4.isDone() && !f5.isDone() && !f6.isDone());
+		} catch(Exception e){
+			fail("Exception occured while opening connections to host", e);
+		}
+		
+		//Should be no log warnings here
+		
+    }
+	
+	@Test(groups = "standalone")
+    public void testTooManyConnectionsMultiHost() throws IOException {
+		AsyncHttpClientConfig cf = new AsyncHttpClientConfig.Builder().setKeepAlive(true).setMaximumConnectionsPerHost(2).build();
+        AsyncHttpClient ahc = new AsyncHttpClient(cf);
+		
+		try{
+			Future<Response> f1 = ahc.prepareGet(getTargetUrl()).execute();
+			Future<Response> f2 = ahc.prepareGet(getTargetUrl()).execute();
+			Future<Response> f3 = ahc.prepareGet("http://www.google.com/").execute();
+			while(!f1.isDone() && !f2.isDone() && !f3.isDone());
+		} catch(Exception e){
+			fail("Exception occured while opening connections to host", e);
+		}
+		
+		//Should be no log warnings here
+		
+		try{
+			Future<Response> f4 = ahc.prepareGet(getTargetUrl()).execute();
+			Future<Response> f5 = ahc.prepareGet(getTargetUrl()).execute();
+			Future<Response> f6 = ahc.prepareGet(getTargetUrl()).execute();
+			Future<Response> f7 = ahc.prepareGet("http://www.google.com/").execute();
+			while(!f4.isDone() && !f5.isDone() && !f6.isDone() && !f7.isDone());
+		} catch(Exception e){
+			fail("Exception occured while opening connections to host", e);
+		}
+		
+		//Should be log warnings here
+		
     }
 }
